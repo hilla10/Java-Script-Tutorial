b91c921f0eaacffa745827adf2bc033fd997954b
hilla10 <hillaman592@gmail.com>
2024-03-29 16:27:05 -1100

    added

---
+++ 08-javascript-under-the-hood/script.js
@@ -0,0 +1,69 @@
+/* 
+function first() {
+  console.log('first...');
+  second();
+}
+
+function second() {
+  console.log('second...');
+  third();
+}
+
+function third() {
+  console.log('third...');
+}
+
+first();
+ */
+
+/* let x = 100;
+let y = 50;
+
+console.log(getSum(x, y));
+
+function getSum(n1, n2) {
+  var sum = n1 + n2;
+
+  return sum;
+} */
+
+// console.log(sum1, sum2);
+
+// Asynchronous
+
+// setTimeout(() => {
+//   console.log('Hello from callback');
+// }, 2000);
+
+// console.log('Run me!');
+
+// Memory Storage
+
+// Primitive types: Stored on the stack
+let name = 'John';
+let age = 3;
+
+// Reference types: stored on the heap
+/* 
+let person = {
+  name: 'Brad',
+  age: 30,
+};
+
+let newName = name;
+newName = 'Jonathan';
+
+let newPerson = person;
+
+newPerson.name = 'Bradly';
+
+console.log(newPerson, person);
+
+function Person(name) {
+  this.name = name;
+}
+
+const person1 = new Person('John');
+const person2 = new Person('Jane');
+const person3 = new Person('Bob');
+ */
